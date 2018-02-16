eab87217e3bf4de2994d4e6bde8c07aa24d1055f
Kayne Ruse <kayneruse@gmail.com>
2018-02-17 10:03:02 +1100

    Everything seems broken now

---
+++ Assets/Scripts/Path2D.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Path2D {
+	public readonly Vector2[] lookPoints;
+	public readonly Line2D[] turnBoundries;
+	public readonly int finishLineIndex;
+
+	public Path2D(Vector2[] waypoints, Vector2 startPos, float turnDistance) {
+		lookPoints = waypoints;
+		turnBoundries = new Line2D[lookPoints.Length];
+		finishLineIndex = turnBoundries.Length - 1;
+
+		Vector2 previousPoint = startPos;
+		for (int i = 0; i < lookPoints.Length; i++) {
+			Vector2 currentPoint = lookPoints [i];
+			Vector2 dirToCurrentPoint = (currentPoint - previousPoint).normalized;
+			Vector2 turnBoundryPoint = (i == finishLineIndex) ? currentPoint : currentPoint - dirToCurrentPoint * turnDistance;
+			turnBoundries [i] = new Line2D (turnBoundryPoint, previousPoint - dirToCurrentPoint * turnDistance);
+			previousPoint = turnBoundryPoint;
+		}
+	}
+
+	public void DrawWithGizmos() {
+		Gizmos.color = Color.black;
+		foreach(Vector2 p in lookPoints) {
+			Gizmos.DrawCube (new Vector3 (p.x, p.y, -2), Vector3.one);
+		}
+
+		Gizmos.color = Color.white;
+		foreach(Line2D l in turnBoundries) {
+			l.DrawWithGizmos (5);
+		}
+	}
+}
--- Assets/Scripts/Unit2D.cs
+++ Assets/Scripts/Unit2D.cs
@@ -5,51 +5,32 @@ using UnityEngine;
 public class Unit2D : MonoBehaviour {
 
 	public Transform target;
-	float speed = 1;
-	Vector2[] path;
-	int targetIndex;
+	public float speed = 1;
+	public float turnDistance = 0.5f;
+
+	Path2D path;
 
 	void Start() {
 		PathRequestManager.RequestPath (transform.position, target.transform.position, OnPathFound);
 	}
 
-	public void OnPathFound(Vector2[] newPath, bool success) {
+	public void OnPathFound(Vector2[] waypoints, bool success) {
 		if (success) {
-			path = newPath;
+			path = new Path2D(waypoints, transform.position, turnDistance);
 			StopCoroutine ("FollowPath");
 			StartCoroutine ("FollowPath");
 		}
 	}
 
 	IEnumerator FollowPath() {
-		targetIndex = 0;
-		Vector2 currentWaypoint = path [0];
 		while(true) {
-			if (new Vector2(transform.position.x, transform.position.y) == currentWaypoint) {
-				targetIndex++;
-				if (targetIndex >= path.Length) {
-					yield break;
-				}
-				currentWaypoint = path [targetIndex];
-			}
-			transform.position = Vector2.MoveTowards (transform.position, currentWaypoint, speed * Time.deltaTime);
 			yield return null;
 		}
 	}
 
 	public void OnDrawGizmos() {
 		if (path != null) {
-			for (int i = targetIndex; i < path.Length; i++) {
-				Gizmos.color = Color.black;
-				Gizmos.DrawCube (new Vector3 (path [i].x, path [i].y, -1), Vector3.one);
-
-				if (i == targetIndex) {
-					Gizmos.DrawLine (transform.position, new Vector3 (path [i].x, path [i].y, -1));
-				}
-				else {
-					Gizmos.DrawLine (new Vector3 (path [i - 1].x, path [i - 1].y, -1), new Vector3 (path [i].x, path [i].y, -1));
-				}
-			}
+			path.DrawWithGizmos ();
 		}
 	}
 }
