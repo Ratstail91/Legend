531156edb5891ddc9ec7abf4771639341202a955
Kayne Ruse <kayneruse@gmail.com>
2018-02-17 09:04:56 +1100

    Implemented smooth weights

--- Assets/Scripts/NodeGrid2D.cs
+++ Assets/Scripts/NodeGrid2D.cs
@@ -8,6 +8,7 @@ public class NodeGrid2D : MonoBehaviour {
 	public LayerMask unwalkableMask;
 	public Vector2 worldGridSize; //real size of the grid
 	public float nodeRadius;
+	public int obsticleProximityPenalty = 10;
 	public TerrainType[] walkableRegions;
 	Dictionary<int, int> walkableRegionsDictionary = new Dictionary<int, int> (); //optimization
 
@@ -16,6 +17,9 @@ public class NodeGrid2D : MonoBehaviour {
 	Node2D[,] grid;
 	float nodeDiameter;
 	int gridSizeX, gridSizeY; //the number of nodes stored on an axis
+
+	int minPenalty = int.MaxValue;
+	int maxPenalty = int.MinValue;
 
 	void Awake() {
 		nodeDiameter = nodeRadius * 2;
@@ -41,7 +45,8 @@ public class NodeGrid2D : MonoBehaviour {
 		Gizmos.DrawWireCube (transform.position, new Vector3 (worldGridSize.x, worldGridSize.y, 1));
 		if (grid != null && displayGridGizmos) {
 			foreach(Node2D n in grid) {
-				Gizmos.color = (n.walkable ? Color.white : Color.red);
+				Gizmos.color = Color.Lerp (Color.white, Color.black, Mathf.InverseLerp (minPenalty, maxPenalty, n.penalty));
+				Gizmos.color = (n.walkable ? Gizmos.color : Color.red);
 				Gizmos.DrawCube (new Vector3(n.worldPosition.x, n.worldPosition.y, 1), Vector3.one * (nodeDiameter - 0.8f));
 			}
 		}
@@ -72,14 +77,71 @@ public class NodeGrid2D : MonoBehaviour {
 
 				int movementPenalty = 0;
 
-				if (walkable) {
-					RaycastHit2D hit = Physics2D.Raycast (worldPoint, Vector2.zero, 0, walkableMask);
-					if (hit != null && hit.collider != null) {
-						walkableRegionsDictionary.TryGetValue (hit.collider.gameObject.layer, out movementPenalty);
-					}
+				RaycastHit2D hit = Physics2D.Raycast (worldPoint, Vector2.zero, 0, walkableMask);
+				if (hit != null && hit.collider != null) {
+					walkableRegionsDictionary.TryGetValue (hit.collider.gameObject.layer, out movementPenalty);
+				}
+
+				if (!walkable) {
+					movementPenalty += obsticleProximityPenalty;
 				}
 
 				grid [i, j] = new Node2D (walkable, worldPoint, i, j, movementPenalty);
+			}
+		}
+
+		BlurPenaltyMap (3);
+	}
+
+	void BlurPenaltyMap(int blurSize) {
+		int kernelSize = blurSize * 2 + 1;
+		int kernelExtents = (kernelSize - 1) / 2;
+
+		int[,] penaltiesHorizontalPass = new int[gridSizeX,gridSizeY];
+		int[,] penaltiesVerticalPass = new int[gridSizeX,gridSizeY];
+
+		//horizontal pass
+		for (int y = 0; y < gridSizeY; y++) {
+			//row 0
+			for (int x = -kernelExtents; x <= kernelExtents; x++) {
+				int sampleX = Mathf.Clamp(x, 0, kernelExtents);
+				penaltiesHorizontalPass[0,y] += grid[sampleX, y].penalty;
+			}
+
+			for (int x = 1; x < gridSizeX; x++) {
+				int removeIndex = Mathf.Clamp(x - kernelExtents - 1, 0, gridSizeX);
+				int addIndex = Mathf.Clamp(x + kernelExtents, 0, gridSizeX -1);
+
+				penaltiesHorizontalPass[x,y] = penaltiesHorizontalPass[x-1, y] - grid[removeIndex,y].penalty + grid[addIndex,y].penalty;
+			}
+		}
+
+		//vertical pass
+		for (int x = 0; x < gridSizeX; x++) {
+			//row 0
+			for (int y = -kernelExtents; y <= kernelExtents; y++) {
+				int sampleY = Mathf.Clamp(x, 0, kernelExtents);
+				penaltiesVerticalPass[x,0] += penaltiesHorizontalPass[x, sampleY];
+			}
+
+			int blurredPenalty = Mathf.RoundToInt((float)penaltiesVerticalPass [x, 0] / (kernelSize * kernelSize));
+			grid [x, 0].penalty = blurredPenalty;
+
+			for (int y = 1; y < gridSizeY; y++) {
+				int removeIndex = Mathf.Clamp(y - kernelExtents - 1, 0, gridSizeY);
+				int addIndex = Mathf.Clamp(y + kernelExtents, 0, gridSizeY -1);
+
+				penaltiesVerticalPass[x,y] = penaltiesVerticalPass[x, y-1] - penaltiesHorizontalPass[x,removeIndex] + penaltiesHorizontalPass[x,addIndex];
+				blurredPenalty = Mathf.RoundToInt((float)penaltiesVerticalPass [x, y] / (kernelSize * kernelSize));
+				grid [x, y].penalty = blurredPenalty;
+
+				//gizmo stuff
+				if (blurredPenalty > maxPenalty) {
+					maxPenalty = blurredPenalty;
+				}
+				if (blurredPenalty < minPenalty) {
+					minPenalty = blurredPenalty;
+				}
 			}
 		}
 	}
